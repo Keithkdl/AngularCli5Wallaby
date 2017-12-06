6d7571f5367ac103fbe63ca1895f01c6ef0ebd3e
Keith Lyon <Keith@DamodeSoftware.com>
2017-12-06 19:05:15 +0000

    using tsconfig @app errors wallabyjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { environment } from '../environments/environment';
+import { environment } from '@env/environment';                     // error
+// import { environment } from '../environments/environment';       // works
 
 @Component({
     selector: 'app-root',
